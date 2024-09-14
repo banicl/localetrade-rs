adaa898cee81ae65fdd7f07100f5e4cbed801d32
Leonarda Banić <lbanic@Leonardas-MacBook-Air.local>
2024-09-14 11:50:13 +0200

    Additional Frontend Update

--- frontend/src/components/NewsPage.vue
+++ frontend/src/components/NewsPage.vue
@@ -69,11 +69,6 @@
 
 import axios from 'axios';
 
-import image1 from '@/assets/image1.jpg';
-import image2 from '@/assets/image2.jpg';
-import image3 from '@/assets/image3.jpg';
-import image4 from '@/assets/image4.jpg';
-
 export default {
   name: 'NewsPage',
   data() {
